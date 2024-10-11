7759a9d44354a146c1c67a2f8e9e4d231105b84d
MR. YE PHONE KYAW <66130500830@st.sit.kmutt.ac.th>
2024-10-11 23:41:31 +0700

    exposed server to all origins for dev

--- core/config/corsConfig.js
+++ core/config/corsConfig.js
@@ -2,11 +2,14 @@ const allowedOrigins = ["http://localhost:5173"];
 
 export const corsConfig = {
   origin: (origin, callback) => {
-    if (origin && allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    // if (origin && allowedOrigins.indexOf(origin) !== -1) {
+    //   callback(null, true);
+    // } else {
+    //   callback(new Error("Not allowed by CORS"));
+    // }
+
+    // allow all origins just for dev environment
+    callback(null, true);
   },
   allowedHeaders: ["Content-Type", "Authorization", "authorization"],
   optionsSuccessStatus: 200,
