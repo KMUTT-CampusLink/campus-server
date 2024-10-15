a691b65c900fbc87e36590d260a4a4e8b18d0ed8
Hein <nwaimarn@gmail.com>
2024-10-15 10:59:43 +0700

    backend-ready

--- modules/registration/routes/regisRouter.js
+++ modules/registration/routes/regisRouter.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-// import your logics from controllers here
-
+import { studentRouter } from "./studentRouter.js";
+import { courseRouter } from "./courseRouter.js";
+import { enrollmentRouter } from "./enrollmentRouter.js";
+import { transcriptRouter } from "./transcriptRouter.js";
+import { gpaxRouter } from "./gpaxRouter.js";
 const regisRouter = Router();
 
-// create routes here
 regisRouter.get("/", (req, res) => {
   return res.send("Registration");
 });
+regisRouter.use("/student", studentRouter);
+regisRouter.use("/course", courseRouter);
+regisRouter.use("/enroll", enrollmentRouter);
+regisRouter.use("/transcript", transcriptRouter);
+regisRouter.use("/gpax", gpaxRouter);
 
 export { regisRouter };
