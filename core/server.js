1cf928134c6a57df4f16691d6b92e3a43aeb5800
MR. SWAN HTET NAING <66130500823@st.sit.kmutt.ac.th>
2024-10-20 14:35:25 +0700

    backendAdded

--- core/server.js
+++ core/server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 
 // all routing
 app.use("/api/users", userRouter);
-app.use(verifyAccessToken);
+//app.use(verifyAccessToken); **;//to uncomment before push**
 app.get("/api/authorize", (req, res) => {
   return res.status(200).json({
     condition: "success",
--- modules/clubs/routes/clubRouter.js
+++ modules/clubs/routes/clubRouter.js
@@ -1,11 +1,27 @@
 import { Router } from "express";
 // import your logics from controllers here
+import { getAllStudents, getStudentbyId, getAllClubs, createClub, getClubbyId, requestToJoinClub } from "../controllers/club.js"; // Import the controller
+import multer from "multer";
 
-const clubRouter = Router();
-
-// create routes here
-clubRouter.get("/", (req, res) => {
-  return res.send("Clubs Management");
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname); 
+    }
 });
 
-export { clubRouter };
+const upload = multer({ storage }); // Configure multer storage
+
+const router = Router();
+
+router.get("/students", getAllStudents);
+router.get("/students/:id", getStudentbyId);
+router.post("/clubs/:clubId/request", requestToJoinClub);
+
+router.get("/", getAllClubs);
+router.get("/:id", getClubbyId);
+router.post("/create", upload.single('club_img'), createClub);
+export { router as clubRouter };
