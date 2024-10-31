896a1b49a3fc93f6d630b603da15e1e2a6da027c
Ayechanaung7777 <ayechanaung415@gmail.com>
2024-10-31 22:55:19 +0700

    controllerFiles Updated

---
+++ modules/chatbot/controllers/webhookReq/programs/requiredCourseController.js
@@ -0,0 +1,26 @@
+import prisma from "../../../../../core/db/prismaInstance.js";
+
+export const requirecourseController = async (req, res) => {
+    const { name: progName } = req.query;
+  try {
+    
+    const courses = await prisma.$queryRaw`
+      Select c.name as coursename--p.name as programname
+      From course as c,program as p
+      Where c.program_id=p.id and p.name = ${progName}
+     
+    `;
+    
+    let fulfillment = `The required course for  "${progName}" are. \n`;
+    courses.map((course, index) => {
+      fulfillment += `${index + 1}. ${course.coursename} 
+        
+      \n`;
+    });
+    
+    return res.json({ fulfillment });
+  } catch (error) {
+    console.error("Error fetching required course: " + error);
+    res.status(500).json({ error: "Failed to fetch required courses" });
+  }
+};
