d2b17ffbed5034b012a3c123f1e978f24a0594f4
Hejdula <hejdula@proton.me>
2024-11-01 21:23:30 +0700

    booking safety checks

--- modules/transportation/controllers/user/book.js
+++ modules/transportation/controllers/user/book.js
@@ -2,6 +2,26 @@ import prisma from "../../../../core/db/prismaInstance.js";
 import errorHandler from "../../utils/errorHandler.js";
 
 export const bookForTrip = errorHandler(async (req, res) => {
+  if (!req.user) {
+    throw new UnauthorizedError();
+  }
+  if (!req.body.tripID) {
+    throw new BadRequestError("Trip ID is missing");
+  }
+
+  await prisma.trip_booking
+    .findFirst({
+      where: {
+        user_id: req.user.id,
+        trip_id: req.body.tripID,
+      },
+    })
+    .then((booking) => {
+      if (booking) {
+        return res.json({ booking, message: "This booking already exists" });
+      }
+    });
+
   await prisma.trip_booking
     .create({
       data: {
@@ -10,7 +30,7 @@ export const bookForTrip = errorHandler(async (req, res) => {
       },
     })
     .then((booking) => {
-      return res.json({ booking });
+      return res.json({ booking, message: "Booking successful" });
     })
     .catch((err) => {
       console.log(err);
