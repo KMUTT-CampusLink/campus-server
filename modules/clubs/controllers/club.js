c91cee559e84da8facadfd69703cc868d4533128
Thin Nwe Soe <thinnwesoe2001@gmail.com>
2024-11-04 17:11:05 +0700

    update creating club

--- modules/clubs/controllers/club.js
+++ modules/clubs/controllers/club.js
@@ -20,6 +20,29 @@ export const getAllStudents = async (req, res) => {
   }
 };
 
+//Fetch professors for creating a club
+export const getAllProfessors = async (req, res) => {
+  try {
+    const professors = await prisma.employee.findMany({
+      where: {
+        job_title: "Professor",
+      },
+      select: {
+        id: true,
+        firstname: true,
+        midname: true,
+        lastname: true,
+      },
+    });
+    res.status(200).json({ success: true, data: professors });
+  } catch (error) {
+    console.error("Failed to fetch professors: ", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Failed to fetch professors" });
+  }
+};
+
 export const getStudentbyId = async (req, res) => {
   const { id } = req.params;
   try {
@@ -41,6 +64,7 @@ export const getStudentbyId = async (req, res) => {
       .json({ success: false, message: "Failed to fetch student" });
   }
 };
+
 // Fetch all clubs for landing page
 export const getAllClubs = async (req, res) => {
   try {
@@ -85,18 +109,20 @@ export const getClubbyId = async (req, res) => {
 
 // Create a new club
 export const createClub = async (req, res) => {
-  // const {  description, content,owner_id } = req.body;
   const name = req.body.clubName;
   const description = req.body.clubDescription;
   const content = req.body.clubDetails;
   const owner_id = "STU00023";
   const members = req.body.members;
+  const building_id = req.body.buildingId;
+
   console.log(name);
   console.log(description);
   console.log(content);
   console.log(owner_id);
   console.log(members);
-  // const owner_id=STU00023;
+  console.log(building_id);
+
   const club_img = req.file
     ? req.file.filename
     : "https://img.freepik.com/premium-vector/badminton-vintage-logo-design-perfect-team-badminton-club-badminton-championship_297778-707.jpg"; // Get the uploaded file's name
@@ -109,53 +135,120 @@ export const createClub = async (req, res) => {
   }
 
   try {
+    const existingClub = await prisma.club.findUnique({
+      where: {
+        name,
+      },
+    });
+  
+  if (existingClub) {
+    return res.status(400).json({
+      success: false,
+      message: "A club with this name already exists.",
+    });
+  }
+
     // Create the new club with the uploaded image
     const newClub = await prisma.club.create({
       data: {
-        name,
-        description,
-        content,
-        club_img, // Store the filename in the database
-        owner_id, // Links to the 'student' model
+        name: name,
+        description: description,
+        content: content,
+        club_img: club_img,
+        owner_id: owner_id, // Links to the 'student' model
+        building_id: parseInt(building_id),
       },
     });
+    console.log("New club created:", newClub);
     // Prase the members array as it is sent as a string
     const addedMembers = JSON.parse(members);
     // Optionally, add the owner as a club member with admin rights
+    if(owner_id.startsWith("STU")){
     await prisma.club_member.create({
       data: {
         club_id: newClub.id,
         student_id: owner_id,
-        is_admin: true, // Set the owner as the admin
+        is_admin: true,
       },
     });
+  } else if(owner_id.startsWith("EMP")){
+    await prisma.club_member.create({
+      data: {
+        club_id: newClub.id,
+        professor_id: owner_id,
+        is_admin: true,
+      },
+    });
+  } else {
+    console.warn(`Unrecognized owner ID format: ${owner_id}`);
+  }
+    console.log("Added club owner as memeber.");
 
     for (const memberId of addedMembers) {
-      await prisma.club_member.create({
-        data: {
-          club_id: newClub.id,
-          student_id: memberId,
-          is_admin: false, // Regular member
-        },
-      });
+      console.log(`Adding member: ${memberId}`);
+      if (memberId.startsWith("STU")) {
+        await prisma.club_member.create({
+          data: {
+            club_id: newClub.id,
+            student_id: memberId,
+            is_admin: false,
+          },
+        });
+      } else if (memberId.startsWith("EMP")) {
+        await prisma.club_member.create({
+          data: {
+            club_id: newClub.id,
+            employee_id: memberId,
+            is_admin: false,
+          },
+        });
+      } else {
+        console.warn(`Unrecognized member ID format: ${memberId}`);
+      }
     }
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "Club created successfully.",
-        data: newClub,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "Club created successfully.",
+      data: newClub,
+    });
   } catch (error) {
     console.error("Error creating club:", error);
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: "An error occurred while creating the club.",
-      });
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while creating the club.",
+    });
   }
 };
+
+// Update description of a club
+export const updateClubDescription = async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const { content } = req.body;
+
+  try {
+    const updatedClub = await prisma.club.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        description: description,
+        content: content,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Club description updated.",
+      data: updatedClub,
+    });
+  } catch (error) {
+    console.error("Error updating club description:", error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while updating the club description.",
+    });
+  }
+}
 
 // Create a new post
 export const createPost = async (req, res) => {
@@ -207,12 +300,10 @@ export const requestToJoinClub = async (req, res) => {
     });
 
     if (existingMember) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Already a member or pending request",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "Already a member or pending request",
+      });
     }
 
     // Add a new entry to the club_member table (join request)
--- modules/clubs/routes/clubRouter.js
+++ modules/clubs/routes/clubRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 // import your logics from controllers here
-import { getAllStudents, getStudentbyId, getAllClubs, createClub, getClubbyId, requestToJoinClub, createPost} from "../controllers/club.js"; // Import the controller
+import { getAllStudents, getStudentbyId, getAllClubs, createClub, getClubbyId, requestToJoinClub, createPost, getAllProfessors, updateClubDescription} from "../controllers/club.js"; // Import the controller
+import { getAllBuildings } from "../controllers/building.js";
 import multer from "multer";
 
 // Multer configuration for file uploads
@@ -18,11 +19,15 @@ const upload = multer({ storage }); // Configure multer storage
 const router = Router();
 
 router.get("/students", getAllStudents);
+router.get("/professors", getAllProfessors);
+router.get("/buildings", getAllBuildings);
 router.get("/students/:id", getStudentbyId);
 router.post("/clubs/:clubId/request", requestToJoinClub);
 
 router.get("/", getAllClubs);
 router.get("/:id", getClubbyId);
+router.put("/:id", updateClubDescription);
 router.post("/create", upload.single('club_img'), createClub);
 router.post("/admin/post", upload.single("photo"), createPost);
+
 export { router as clubRouter };
