6f5893036ba22ff9eda6284c84943cee31aedf12
Swan Htet Naing <swanhtetnaing065@gmail.com>
2024-11-08 15:43:24 +0700

    handleRequests

--- modules/clubs/routes/clubRouter.js
+++ modules/clubs/routes/clubRouter.js
@@ -9,13 +9,17 @@ import {
   requestToJoinClub,
   createPost,
   getAllProfessors,
-  updateClubDescription
+  updateClubDescription,
+  getPendingRequests,
+  updateJoinRequestStatus
 } from "../controllers/club.js";
 
-import { getAllPosts } from "../controllers/post.js";
-import { getAllAnnouncements, createAnnouncement } from "../controllers/announcement.js";
+import { getAllPosts, getPostByClubId } from "../controllers/post.js";
+import { getAllAnnouncements, createAnnouncement, getAnnouncementsByClubId } from "../controllers/announcement.js";
 import { getAllBuildings } from "../controllers/building.js";
 import { clubLocation } from "../controllers/clubLocation.js";
+import { getMemberByClubId } from "../controllers/clubMember.js";
+import { getNotifications } from "../controllers/clubNotifications.js";
 
 // Multer configuration for file uploads
 const storage = multer.diskStorage({
@@ -37,8 +41,10 @@ router.get("/professors", getAllProfessors);
 router.get("/buildings", getAllBuildings);
 
 router.get("/posts", getAllPosts); // More specific path
+router.get("/posts/:clubId", getPostByClubId);
 router.get("/announcements", getAllAnnouncements); // More specific path
-router.post("/announcements", createAnnouncement); // Route to create an announcement
+router.get("/announcements/:clubId", getAnnouncementsByClubId);
+router.get("/members/:clubId", getMemberByClubId);
 
 router.get("/:id", getClubbyId);
 router.put("/:id", updateClubDescription);
@@ -46,8 +52,14 @@ router.post("/:clubId/join-request", requestToJoinClub);
 router.get("/clubLocation/:buildingId", clubLocation); // Path to get clubs by building ID
 
 router.post("/create", upload.single("club_img"), createClub);
-router.post("/admin/post", upload.single("photo"), createPost);
+router.post("/admin/post/:clubId", upload.single("photo"), createPost);
+router.post("/admin/announcements/:clubId", upload.none(), createAnnouncement); // Route to create an announcement
 
 router.get("/", getAllClubs);
+
+router.get("/:clubId/pending-requests", getPendingRequests);
+router.put("/:clubId/members/:memberId/status", updateJoinRequestStatus);
+
+router.get("/notifications", getNotifications);
 
 export { router as clubRouter };
