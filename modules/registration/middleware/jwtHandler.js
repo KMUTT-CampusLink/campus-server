b7586bad68b3a0bd267dee07eb546799a3d07476
MR. YE PHONE KYAW <66130500830@st.sit.kmutt.ac.th>
2024-11-08 10:35:23 +0700

    setup s2s authentication

--- modules/registration/middleware/jwtHandler.js
+++ modules/registration/middleware/jwtHandler.js
@@ -1,13 +1,24 @@
 import jwt from "jsonwebtoken";
 
 const verifyAccessToken = (req, res, next) => {
-  const token = req.cookies.token;
+  // request coming from server
+  const authHeader = req.headers.Authorization || req.headers.authorization;
+
+  // request coming from dialogflow cx
+  const sessionId = null;
+
+  const token =
+    req.cookies.token ||
+    (authHeader &&
+      authHeader?.startsWith("Bearer ") &&
+      authHeader.split(" ")[1]);
   if (!token) {
     return res.status(401).send("Unauthorized access");
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.token = token;
     req.user = {
       id: decoded.id,
       campus_email: decoded.campus_email,
