e6b95c801527866b485a6c1fbfa3b206e4462be7
Delods <dadelous.d@gmail.com>
2024-11-09 20:38:23 +0700

    This is backend of Room booking System

--- modules/building-security/routes/secureRouter.js
+++ modules/building-security/routes/secureRouter.js
@@ -1,11 +1,35 @@
 import { Router } from "express";
-// import your logics from controllers here
+import {
+  fetchBuildingData,
+  fetchFloorsByBuildingId,
+  fetchRoomsByFloorId,
+  createBooking,
+  fetchBooked,
+  deleteBookingController,
+  fetchAvailableTimesController,
+} from "../buildingController.js";
 
 const secureRouter = Router();
 
-// create routes here
-secureRouter.get("/", (req, res) => {
-  return res.send("Building and Security");
-});
+// Route to fetch buildings
+secureRouter.get("/buildings", fetchBuildingData);
+
+// Route to fetch floors by building ID
+secureRouter.get("/floors/:buildingId", fetchFloorsByBuildingId);
+
+// Route to fetch rooms by floor ID
+secureRouter.get("/rooms/:floorId", fetchRoomsByFloorId);
+
+// Route to fetch rooms from room_booking table that was reserved
+secureRouter.get("/getBooked", fetchBooked);
+
+// Route to get available time to calculate
+secureRouter.get("/getAvailableTimes", fetchAvailableTimesController);
+
+// Route to create a booking with time overlap check
+secureRouter.post("/bookings", createBooking);
+
+// Route to delete a booking by id that selected by delete icon
+secureRouter.delete("/bookings/:id", deleteBookingController);
 
 export { secureRouter };
