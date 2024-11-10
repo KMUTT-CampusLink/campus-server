ffcf6a341dd093ee336714789a7880220c23c6b3
Hein <nwaimarn@gmail.com>
2024-11-10 18:57:31 +0700

    st profile function added

--- modules/registration/controllers/studentController.js
+++ modules/registration/controllers/studentController.js
@@ -1,6 +1,6 @@
 import prisma from "../../../core/db/prismaInstance.js";
 
-export default async function studentController(req, res) {
+export const studentInfoController = async (req, res) => {
   const { studentId } = req.params;
 
   try {
@@ -22,6 +22,49 @@ export default async function studentController(req, res) {
         program p ON d.program_id = p.id
       JOIN 
         faculty f ON p.faculty_id = f.id
+      WHERE 
+        s.id = ${studentId};
+    `;
+
+    if (result.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    const student = result[0];
+    res.json(student);
+  } catch (error) {
+    console.error("Error fetching student data:", error);
+    res.status(500).json({ error: "Failed to fetch student data" });
+  }
+}
+
+export const studentProfileController = async (req, res) => {
+  const { studentId } = req.params;
+
+  try {
+    const result = await prisma.$queryRaw`
+      SELECT 
+        personal_email,
+        firstname,
+        midname,
+        lastname,
+        phone,
+        address,
+        date_of_birth,
+        identification_no,
+        degree_level
+      FROM 
+        student s
+      JOIN 
+        program_batch pb ON s.program_batch_id = pb.id
+      JOIN 
+        degree d ON pb.degree_id = d.id
+      JOIN 
+        program p ON d.program_id = p.id
+      JOIN 
+        faculty f ON p.faculty_id = f.id
+      JOIN
+        "user" u ON u.id = s.user_id
       WHERE 
         s.id = ${studentId};
     `;
