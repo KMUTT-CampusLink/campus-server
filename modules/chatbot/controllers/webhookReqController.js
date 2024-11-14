cfc4365e67938722071caa1fee7e0c8feb287b3a
shinemin19 <shinemin.khan@kmutt.ac.th>
2024-11-14 18:38:22 +0700

    to pull

--- modules/chatbot/controllers/webhookReqController.js
+++ modules/chatbot/controllers/webhookReqController.js
@@ -10,10 +10,16 @@ import { tutionFeeController } from "./webhookReq/programs/tutionFeeController.j
 export const webhookReqController = async(req, res) => {
   const pageName = req.body.pageInfo.displayName;
   let result;
+  let paramsList = {};
+  console.log(req.body.sessionInfo.parameters);
   if(pageName === "Fees"){
     const programName = req.body.sessionInfo.parameters.program.trim();
     const degreeLevel = req.body.sessionInfo.parameters.degreelevel.trim() + " Degree";
     result = await tutionFeeController(programName, degreeLevel);
+    paramsList = {
+      "program": null,
+      "degreelevel": null,
+    }
   }
   else if(pageName === "Programs List") {
     result = await programsListController();
@@ -22,7 +28,13 @@ export const webhookReqController = async(req, res) => {
   }else if(pageName === "EventList"){
     result = await libraryEventController();
   }else if(pageName === "Course"){
-    result = await requirecourseController();
+    const progName = req.body.sessionInfo.parameters.program.trim();
+    const degreeLevel = req.body.sessionInfo.parameters.degreelevel.trim();
+    result = await requirecourseController(progName, degreeLevel);
+    paramsList = {
+      "program": null,
+      "degreelevel": null,
+    };
   }else if(pageName === "Semester Starttime"){
     result = await semesterStartController();
   }else if(pageName === "Semester Endingtime"){
@@ -32,6 +44,9 @@ export const webhookReqController = async(req, res) => {
   }else if(pageName === "Member"){
     const clubName = req.body.sessionInfo.parameters.clubs.trim();
     result = await clubMemberController(clubName);
+    paramsList = {
+      "clubs": null,
+    }
   }
 
   res.json({
@@ -43,6 +58,9 @@ export const webhookReqController = async(req, res) => {
           }
         }
       ]
+    },
+    "sessionInfo": {
+      "parameters": paramsList
     }
   });
 }
