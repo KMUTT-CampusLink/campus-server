a62205ab92a49f09c17260880c92689076cbe2d9
LilEurey <134917527+LilEurey@users.noreply.github.com>
2024-11-19 16:43:20 +0700

    fixsomething

--- modules/building-security/controllers/addLostAndFoundList.js
+++ modules/building-security/controllers/addLostAndFoundList.js
@@ -1,30 +1,62 @@
 import prisma from "../../../core/db/prismaInstance.js";
+import { decodeToken } from "../middleware/jwt.js"
 
 export const addLostAndFoundList = async (req, res) => {
+    const token = req.cookies.token;
+    const decode = decodeToken(token);
     const {
-        reporter_id,
-        name,
+        floor_id,
         description,
-        found_location,
         status,
         owner_id
     } = req.body;
 
     try {
-        const getThailandTime = () => {
-            const now = new Date();
-            return new Date(now.getTime() + 7 * 60 * 60 * 1000); // Add 7 hours for ICT
-        };
+        const nameData = await prisma.student.findFirst({
+            select: {
+                firstname: true,
+                lastname: true,
+            },
+            where: {
+                user_id: decode.id,
+            },
+        }) || await prisma.employee.findFirst({
+            select: {
+                firstname: true,
+                lastname: true,
+            },
+            where: {
+                user_id: decode.id,
+            },
+        });
+        const buildingId = await prisma.floor.findFirst({
+            select: {
+                building_id: true,
+            },
+            where: {
+                id: floor_id,
+            },
+        })
+
+        // Extract the name if found
+        const name = nameData?.firstname+" "+nameData?.lastname || null;
+
+        if (!name) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found in both student and employee records",
+            });
+        }
+
         const newLostAndFound = await prisma.lost_and_found.create({
             data: {
-                reporter_id,
-                name,
+                reporter_id: decode.id,
+                name: name,
                 description,
-                found_location,
+                // found_location,
                 status,
                 owner_id,
-                created_at: getThailandTime(), // Set current time as creation time
-                updated_at: getThailandTime()  // Set current time as updated time
+                building_id: buildingId.building_id,
             },
         });
 
