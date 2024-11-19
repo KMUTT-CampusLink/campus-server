7742eb87534d4d3a36d21e8ba269b2f1cf2f3b02
Hnin Ei Ei Aung <hnineieiaung112@gmail.com>
2024-11-20 00:02:54 +0700

    g04-feat/08/second-sprint

--- modules/online-course/routes/courseRouter.js
+++ modules/online-course/routes/courseRouter.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import {
+  addVideo,
+  getAllVideos,
+} from "../controllers/courseMaterialController.js";
+import multerErrorHandler from "../../../core/middleware/multerErrorHandler.js";
+import file_uploader from "../../../core/middleware/multerUploader.js";
+import {
   getAllCourses,
   getAllCoursesByProfessorID,
   getAllCoursesByStudentID,
@@ -14,9 +20,16 @@ courseRouter.get("/", (req, res) => {
 });
 
 courseRouter.get("/all", getAllCourses);
+courseRouter.get("/videos", getAllVideos);
 courseRouter.get("/:studentID", getCourseByStudentID);
 courseRouter.get("/:studentID/all", getAllCoursesByStudentID);
 courseRouter.get("/:professorID/teach", getAllCoursesByProfessorID);
 courseRouter.get("/course/:sectionID", getCourseHeaderBySectionID);
+courseRouter.post(
+  "/addVideo",
+  file_uploader.single("courseVideo"),
+  multerErrorHandler,
+  addVideo
+);
 
 export { courseRouter };
