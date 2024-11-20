fdabc3dc11902b38c81f1899ebda23393da9d710
Hein <nwaimarn@gmail.com>
2024-11-20 15:55:20 +0700

    conflict solved

--- core/server.js
+++ core/server.js
@@ -56,15 +56,15 @@ app.use("/api/attend", attendRouter);
 app.use("/api/security", secureRouter);
 app.use("/api/botastra", verifyAccessToken, botRouter);
 app.use("/api/clubs", clubRouter);
-app.use("/api/employ", employRouter);
+app.use("/api/employ", verifyAccessToken, employRouter);
 app.use("/api/library", libRouter);
 app.use("/api/map", mapRouter);
-app.use("/api/courses", verifyAccessToken, courseRouter);
+app.use("/api/courses", courseRouter);
 app.use("/api/exams", examRouter);
-app.use("/api/parking", parkRouter);
-app.use("/api/payment", paymentRouter);
+app.use("/api/parking", verifyAccessToken, parkRouter);
+app.use("/api/payment", verifyAccessToken, paymentRouter);
 app.use("/api/transport", verifyAccessToken, transRouter);
 
 app.listen(port, () =>
-  console.log(`Application started on port ${process.env.HOSTNAME}:${port}`)
+  console.log(`[server] running on port ${process.env.HOSTNAME}:${port}`)
 );
