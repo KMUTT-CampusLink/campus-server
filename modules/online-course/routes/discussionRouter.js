bb62a1ad790e298444142ea00521d9ad3ce435d4
MR. DANIEL BAWM YING <dannybawmying@gmail.com>
2024-11-20 23:48:10 +0700

    com_api

--- modules/online-course/routes/discussionRouter.js
+++ modules/online-course/routes/discussionRouter.js
@@ -8,18 +8,24 @@ import {
   updateDiscussionReply,
   deleteDiscussionReply,
   getAllDiscussionPostsBySectionID,
+  getAllCommentsByPostID,
 } from "../controllers/discussionController.js";
 
 const discussionRouter = Router();
+discussionRouter.get("/view/:postID/comment", getAllCommentsByPostID);
 
-discussionRouter.get("/:sectionID", getAllDiscussionPostsBySectionID)
+discussionRouter.get("/:sectionID", getAllDiscussionPostsBySectionID);
 
 discussionRouter.post("/upload", createDiscussionTopic);
 discussionRouter.put("/:topicId/edit", updateDiscussionTopic);
 discussionRouter.delete("/:topicId/delete", deleteDiscussionTopic);
 
-discussionRouter.post("/comment/create", createDiscussionReply);
+discussionRouter.post("/:topicId/comment/create", createDiscussionReply);
 discussionRouter.put("/comment/:commentId/edit", updateDiscussionReply);
-discussionRouter.delete("/comment/:commentId/delete", deleteDiscussionReply)
+discussionRouter.delete("/comment/:commentId/delete", deleteDiscussionReply);
+
+
+
+
 
 export {discussionRouter}
