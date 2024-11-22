23f1c05c5ef582f137f5c751e707ff14a1aa7477
Ant <antbonekyaw@gmail.com>
2024-11-22 20:35:36 +0700

    22 Nov

--- modules/online-course/routes/assignmentRouter.js
+++ modules/online-course/routes/assignmentRouter.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { createAssignment, deleteAssignment, editAssignment, getAllAssignments } from "../controllers/assignmentController.js";
+import { addAssignmentFile, addSubmissionStudent, deleteAssignment, editAssignment, editSubmissionStudent, getAllAssignments } from "../controllers/assignmentController.js";
 import multerErrorHandler from "../../../core/middleware/multerErrorHandler.js";
 import file_uploader from "../../../core/middleware/multerUploader.js";
-import { addAssignmentFile } from "../controllers/assignmentController2";
 
 const assignmentRouter = Router();
 
-assignmentRouter.post("/create", createAssignment);
 assignmentRouter.put("/:assignmentID/edit", editAssignment);
 assignmentRouter.delete("/:assignmentID/delete", deleteAssignment);
 assignmentRouter.get("/:section_id/all", getAllAssignments);
@@ -17,6 +15,21 @@ assignmentRouter.post(
     file_uploader.single("description"), // Middleware to handle single file upload
     multerErrorHandler, // Middleware to handle upload errors
     addAssignmentFile // Controller to handle file upload and database record creation
+);
+
+// Route to upload student submission
+assignmentRouter.post(
+    "/assignmentSubmission",
+    file_uploader.single("file_path"), // Middleware to handle single file upload
+    multerErrorHandler, // Middleware to handle upload errors
+    addSubmissionStudent // Controller to handle file upload and database record creation
+);
+
+// Route to edit student submission
+assignmentRouter.put(
+    "/:assignmentSubmissionID/submit/edit",
+    file_uploader.single("file_path"), // Middleware for handling the file upload
+    editSubmissionStudent
 );
 
 
