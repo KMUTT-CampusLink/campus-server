d0ad07e699a25924554d603996324fa129128a55
Delods <dadelous.d@gmail.com>
2024-12-16 23:48:20 +0700

    ThisIsGuardReservationBackEnd

--- modules/building-security/routes/secureRouter.js
+++ modules/building-security/routes/secureRouter.js
@@ -8,11 +8,11 @@ import {
   fetchBooked,
   deleteBookingController,
   fetchAvailableTimesController,
+  fetchGuardData,
+  fetchGBuildingData,
+  createGBooking,
 } from "../controllers/buildingController.js";
-// import { getBuilding } from "../controllers/getBuilding.js";
-// import { getFloor } from "../controllers/getFloor.js";
-// import { getRoom } from "../controllers/getRoom.js";
-// import your logics from controllers here
+
 import { getLostAndFoundList } from "../controllers/getLostAndFoundList.js";
 import { getMaintenanceList } from "../controllers/getMaintenanceList.js";
 import { addMaintenanceList } from "../controllers/addMaintenanceList.js";
@@ -21,54 +21,43 @@ import { updateLostAndFoundList } from "../controllers/updateLostAndFoundList.js
 import { adminDeleteMaintenanceList } from "../controllers/adminDeleteMaintenanceList.js";
 import verifyRoles from "../../../core/middleware/verifyRoles.js";
 import { getLostAndFoundInterMap } from "../controllers/LostAndFoundControllerInterMap.js";
-import { getGuardReservationList } from "../controllers/getGuardReservationList.js";
 
-// import { deleteReturned } from "../controllers/deleteReturned.js";
 const secureRouter = Router();
 
-// Route to fetch buildings
 secureRouter.get("/buildings", fetchBuildingData);
 
 secureRouter.get("/buildingsWithRoom", fetchBuildingWithRoomData);
 
-// Route to fetch floors by building ID
 secureRouter.get("/floors/:buildingId", fetchFloorsByBuildingId);
 
-// Route to fetch rooms by floor ID
 secureRouter.get("/rooms/:floorId", fetchRoomsByFloorId);
 
-// Route to fetch rooms from room_booking table that was reserved
 secureRouter.get("/getBooked", fetchBooked);
 
-// Route to get available time to calculate
 secureRouter.get("/getAvailableTimes", fetchAvailableTimesController);
 
-// Route to create a booking with time overlap check
 secureRouter.post("/bookings", createBooking);
 
-// Route to delete a booking by id that selected by delete icon
 secureRouter.delete("/bookings/:id", deleteBookingController);
 
-// secureRouter.get("/building", getBuilding);
-// secureRouter.get("/floor", getFloor);
-// secureRouter.get("/room", getRoom);
 secureRouter.get("/LostAndFoundList", getLostAndFoundList);
+
 secureRouter.get("/MaintenanceList", getMaintenanceList);
-secureRouter.post(
-  "/addMaintenanceList",
-  verifyRoles("Student", "Professor", "Staff"),
-  addMaintenanceList
-);
+
+secureRouter.post("/addMaintenanceList", verifyRoles("Student", "Professor", "Staff"), addMaintenanceList);
+
 secureRouter.post("/addLostAndFoundList", addLostAndFoundList);
+
 secureRouter.patch("/updateStatus/:id", updateLostAndFoundList);
-// secureRouter.delete("/deleteReturned", deleteReturned);
+
 secureRouter.delete("/adminDeleteMaintenanceList/:id", verifyRoles("Professor", "Staff"), adminDeleteMaintenanceList);
 
-// For send API to another GROUP
 secureRouter.get("/lostAndFound/interMap", getLostAndFoundInterMap);
 
+secureRouter.get("/guards", fetchGuardData);
 
+secureRouter.get("/gbuildings", fetchGBuildingData)
 
-
+secureRouter.post("/gbookings", createGBooking)
 
 export { secureRouter };
