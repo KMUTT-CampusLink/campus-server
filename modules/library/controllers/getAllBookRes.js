d5dac751561d2c11ceb428576c6545f26b1e3607
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2024-12-16 11:57:42 +0700

    username of res book

--- modules/library/controllers/getAllBookRes.js
+++ modules/library/controllers/getAllBookRes.js
@@ -2,7 +2,15 @@ import prisma from "../../../core/db/prismaInstance.js";
 
 const getAllBookRes = async (req, res) => {
   try {
-    const bookRes = await prisma.book_reservation.findMany();
+    const bookRes = await prisma.$queryRaw`
+    SELECT br.id id, status, student.firstname, student.lastname, book_duplicate_id, start_date, end_date, unlock_id
+    FROM book_reservation br,student
+    WHERE br.user_id = student.user_id
+    UNION ALL
+    SELECT br.id id, status, employee.firstname, employee.lastname, book_duplicate_id, start_date, end_date, unlock_id
+    FROM book_reservation br,employee
+    WHERE br.user_id = employee.user_id;
+  `;
     res.json(bookRes);
   } catch (error) {
     res.status(500).json({ error: "Error fetching Book Reservation" });
