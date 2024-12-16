48c0098a214657f2c8c0054747245cdfbaa59fc6
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2024-12-16 17:55:03 +0700

    admin qr and dashboard

--- modules/library/controllers/getAllBookRes.js
+++ modules/library/controllers/getAllBookRes.js
@@ -3,13 +3,18 @@ import prisma from "../../../core/db/prismaInstance.js";
 const getAllBookRes = async (req, res) => {
   try {
     const bookRes = await prisma.$queryRaw`
-    SELECT br.id id, status, student.firstname, student.lastname, book_duplicate_id, start_date, end_date, unlock_id
-    FROM book_reservation br,student
-    WHERE br.user_id = student.user_id
-    UNION ALL
-    SELECT br.id id, status, employee.firstname, employee.lastname, book_duplicate_id, start_date, end_date, unlock_id
-    FROM book_reservation br,employee
-    WHERE br.user_id = employee.user_id;
+    SELECT b.cover_image, b.title, bd.id, bd.status, br.firstname, br.lastname
+    FROM book b, book_duplicate bd
+    LEFT JOIN (SELECT br1.book_duplicate_id,s.firstname, s.lastname
+              FROM book_reservation br1, student s
+              WHERE br1.user_id = s.user_id AND br1.status = 'Reserved'
+              UNION
+              SELECT br1.book_duplicate_id,e.firstname, e.lastname
+              FROM book_reservation br1, employee e
+              WHERE br1.user_id = e.user_id AND br1.status = 'Reserved') br
+    ON br.book_duplicate_id = bd.id
+    WHERE b.id = bd.book_id
+    ORDER BY bd.status
   `;
     res.json(bookRes);
   } catch (error) {
