4b9acd2a51ae43acf15bafb7417d1b41f0206e0a
SuppaDuppaa <Escotabo@gmail.com>
2024-12-17 21:03:12 +0700

    Final

--- modules/map/controllers/getAllDepartments.js
+++ modules/map/controllers/getAllDepartments.js
@@ -1,32 +1,48 @@
 import prisma from "../../../core/db/prismaInstance.js";
 
 const getAllDepartments = async (req, res) => {
-    const { campusName } = req.query;
-      
-    try {
-      const campus = await prisma.campus.findFirst({
-        where: {
-          name: campusName
-        }
-      });
-  
-      if (!campus) {
-        return res.status(404).json({ error: "Campus not found" });
+  const { campusName } = req.query;
+
+  try {
+    const campus = await prisma.campus.findFirst({
+      where: {
+        name: campusName
       }
-  
-      const buildings = await prisma.building.findMany({
-        where: {
+    });
+
+    if (!campus) {
+      return res.status(404).json({ error: "Campus not found" });
+    }
+
+    const buildings = await prisma.building.findMany({
+      where: {
+        campus_id: campus.id
+      }
+    });
+
+    const guardSchedule = await prisma.guard_schedule.findMany({
+      where: {
+        building: {
           campus_id: campus.id
         }
-      });
-      
-      res.json({ buildings });
-    } catch (error) {
-      console.error(error); 
-      res.status(500).json({ error: 'Error fetching buildings for the campus' });
-    }
-  };
-  
+      },
+      select: {
+        building_id: true,  
+      }
+    });
+
+    const guardedBuildingIds = new Set(guardSchedule.map(schedule => schedule.building_id));
+
+    const buildingsWithGuardStatus = buildings.map(building => ({
+      ...building,
+      isGuarded: guardedBuildingIds.has(building.id), 
+    }));
+
+    res.json({ buildings: buildingsWithGuardStatus });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error fetching buildings for the campus' });
+  }
+};
+
 export { getAllDepartments };
-
-
